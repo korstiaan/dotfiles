69b8fecc706a623b6e1ac3031cea195d8f48485e
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-01-01 18:56:46 +0100

    Make initialization of asdf autocomplete only active when in when interactive shell

--- private_dot_local/scripts/init_asdf.zsh
+++ private_dot_local/scripts/init_asdf.zsh
@@ -1,5 +1,8 @@
 export ASDF_DATA_DIR="$HOME/.local/asdf"
 
 . "$HOME/.asdf/asdf.sh"
-fpath=("$ASDF_DIR/completions" $fpath)
-autoload -Uz compinit && compinit
+
+if [[ -o login ]]; then
+  fpath=("$ASDF_DIR/completions" $fpath)
+  autoload -Uz compinit && compinit
+fi
