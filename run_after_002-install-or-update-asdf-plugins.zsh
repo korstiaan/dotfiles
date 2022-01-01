d2db988dd8660a2af7fdd641c4c72174a7423392
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-01-01 18:51:05 +0100

    Move asdf init to subprocess as to avoid poluation

--- run_after_002-install-or-update-asdf-plugins.zsh
+++ run_after_002-install-or-update-asdf-plugins.zsh
@@ -1,36 +1,33 @@
 #!/bin/zsh
-set -eo pipefail
+set -euo pipefail
+(
+  set +u;. "$HOME/.local/scripts/init_asdf.zsh"; set -u
 
-. "$HOME/.local/scripts/init_asdf.zsh"
+  export GITHUB_API_TOKEN=${GITHUB_API_TOKEN:-${GITHUB_TOKEN:+$GITHUB_TOKEN}}
 
-set -u
+  install_asdf_plugin() {
+    if ! asdf plugin list | grep -q "^$1$"; then
+      asdf plugin add $1
+    else
+      asdf plugin update $1
+    fi
 
-_asdf_with_env() {
-  GITHUB_API_TOKEN=${GITHUB_API_TOKEN:-${GITHUB_TOKEN:+$GITHUB_TOKEN}} asdf "$@"
-}
+    local version=$2
 
-install_asdf_plugin() {
-  if ! asdf plugin list | grep -q "^$1$"; then
-    _asdf_with_env plugin add $1
-  else
-    _asdf_with_env plugin update $1
-  fi
+    if [ $version = 'latest' ]; then
+      version=$(asdf latest $1)
+    fi
 
-  local version=$2
+    asdf install $1 $version
+    asdf global $1 $version
+  }
 
-  if [ $version = 'latest' ]; then
-    version=$(_asdf_with_env latest $1)
-  fi
-
-  _asdf_with_env install $1 $version
-  _asdf_with_env global $1 $version
-}
-
-install_asdf_plugin ruby latest
-install_asdf_plugin nodejs latest
-install_asdf_plugin python latest
-install_asdf_plugin kubectl latest
-install_asdf_plugin java openjdk-17.0.1
-install_asdf_plugin awscli latest
-install_asdf_plugin golang latest
-install_asdf_plugin helm latest
+  install_asdf_plugin ruby latest
+  install_asdf_plugin nodejs latest
+  install_asdf_plugin python latest
+  install_asdf_plugin kubectl latest
+  install_asdf_plugin java openjdk-17.0.1
+  install_asdf_plugin awscli latest
+  install_asdf_plugin golang latest
+  install_asdf_plugin helm latest
+)
--- run_after_003-setup-fzf.zsh
+++ run_after_003-setup-fzf.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 set -euo pipefail
-
-"$HOME/.local/fzf/install" --key-bindings --completion --no-update-rc --no-bash --no-fish > /dev/null
+(
+  "$HOME/.local/fzf/install" --key-bindings --completion --no-update-rc --no-bash --no-fish > /dev/null
+)
