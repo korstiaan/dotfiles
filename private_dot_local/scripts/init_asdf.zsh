ad279b237d23d246c1b5d3cbb698cc8bc8de7061
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-01-05 11:07:46 +0100

    Use zinit in favour of unmaintained antigen

--- private_dot_local/scripts/init_asdf.zsh
+++ private_dot_local/scripts/init_asdf.zsh
@@ -1,7 +1,4 @@
+export ASDF_DIR="$HOME/.asdf"
 export ASDF_DATA_DIR="$HOME/.local/asdf"
 
 . "$HOME/.asdf/asdf.sh"
-
-if [[ -o login ]]; then
-  fpath=("$ASDF_DIR/completions" $fpath)
-fi
--- run_after_003-setup-fzf.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-set -euo pipefail
-(
-  "$HOME/.local/fzf/install" --key-bindings --completion --no-update-rc --no-bash --no-fish > /dev/null
-)
