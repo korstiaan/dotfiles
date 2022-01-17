ba63d88c258214510e65a48411b9937006be4bb8
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-01-17 13:59:40 +0100

    Use nvim-tree instead of nerdtree

--- dot_config/exact_nvim/exact_lua/mappings.lua
+++ dot_config/exact_nvim/exact_lua/mappings.lua
@@ -1,2 +1,5 @@
 vim.api.nvim_set_keymap('n', '<leader><s-p>',  [[<cmd>Files<CR>]], { noremap = true, })
 vim.api.nvim_set_keymap('n', '<leader><s-f>',  [[:Ag<CR>]], { noremap = true, })
+
+vim.api.nvim_set_keymap('n', '<leader>tt',     [[:NvimTreeToggle<CR>]], { noremap = true, })
+vim.api.nvim_set_keymap('n', '<leader>tr',     [[:NvimTreeRefresh<CR>]], { noremap = true, })
