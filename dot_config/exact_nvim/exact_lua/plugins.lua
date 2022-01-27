8db2380d2fab17e14607f843909bf3b3eb85fc94
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-01-27 12:19:54 +0100

    Add indent-blankline

--- dot_config/exact_nvim/exact_lua/plugins.lua
+++ dot_config/exact_nvim/exact_lua/plugins.lua
@@ -100,6 +100,13 @@ require('packer').startup({
       },
       config = function () require'plugins.lsp-config'.config() end
     }
+
+    use {
+      'lukas-reineke/indent-blankline.nvim',
+      config = function ()
+        require("indent_blankline").setup { }
+      end
+    }
   end,
   config = {
     display = {
