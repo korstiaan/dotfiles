68fa7c2d919fb3ce21e021cb3a31a64e900059a6
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-01-29 09:47:23 +0100

    Add default lspconfig mappings

--- dot_config/exact_nvim/exact_lua/exact_plugins/lsp-config.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/lsp-config.lua
@@ -6,13 +6,40 @@ M.config = function ()
 
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-  lspconfig.tsserver.setup{ capabilities = capabilities }
-  lspconfig.html.setup{ capabilities = capabilities }
-  lspconfig.solargraph.setup{ capabilities = capabilities }
-  lspconfig.pyright.setup{ capabilities = capabilities }
-  lspconfig.sumneko_lua.setup{ capabilities = capabilities }
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  end
+
+  lspconfig.tsserver.setup{ capabilities = capabilities, on_attach = on_attach }
+  lspconfig.html.setup{ capabilities = capabilities, on_attach = on_attach }
+  lspconfig.solargraph.setup{ capabilities = capabilities, on_attach = on_attach }
+  lspconfig.pyright.setup{ capabilities = capabilities, on_attach = on_attach }
+  lspconfig.sumneko_lua.setup{ capabilities = capabilities, on_attach = on_attach }
   lspconfig.omnisharp.setup({
     capabilities = capabilities,
+    on_attach = on_attach,
     cmd = { vim.loop.os_homedir() .. "/.local/share/omnisharp/run", "-lsp" , "--hostPID", tostring(pid) },
     on_exit = function(_code, _signal, _client_id)
       -- omnisharp and/or mono servers don't always get killed, make them anyway
