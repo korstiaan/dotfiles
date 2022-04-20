7997d506e59986275489c9675f533f78b556cab9
Korstiaan de Ridder <korstiaan@korstiaan.com>
2022-04-20 14:32:51 +0200

    Remove gui attribute for buffer highlights

--- dot_config/exact_nvim/exact_lua/exact_plugins/bufferline-config.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/bufferline-config.lua
@@ -33,12 +33,10 @@ M.config = function ()
         guibg = { attribute = "bg", highlight = "StatusLine"}
       },
       buffer_visible = {
-        gui = "",
         guifg = { attribute = "fg", highlight = "Normal" },
         guibg = { attribute = "bg", highlight = "Normal" }
       },
       buffer_selected = {
-        gui = "",
         guifg = { attribute = "fg", highlight = "Normal" },
         guibg = { attribute = "bg", highlight = "Normal" }
       },
